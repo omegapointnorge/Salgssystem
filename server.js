d89db86c67088043e0c2d37d5abd22c194158719
Martin Theodor Vasbotten <mtv@itverket.no>
2021-02-12 11:57:19 +0100

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import express from "express";
 
-const PORT = process.env.HTTP_PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 //Serve static files from client

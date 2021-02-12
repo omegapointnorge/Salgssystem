fa1ee8d5e0482ee9f8b54d0939b0d990568ab98d
Martin Vasbotten <mtv@itverket.no>
2021-02-12 09:28:26 +0100

    test

--- server.compiled.js
+++ server.compiled.js
@@ -6,7 +6,7 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var PORT = process.env.HTTP_PORT || 8080;
+var PORT = process.env.HTTP_PORT || 4001;
 var app = (0, _express["default"])(); //Serve static files from client
 
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build'))); //API

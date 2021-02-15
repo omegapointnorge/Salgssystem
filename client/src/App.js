a540d2d79933b4089e6eace3a859162369a5a735
Martin Theodor Vasbotten <mtv@itverket.no>
2021-02-15 11:09:42 +0100

    ny client og api

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,78 @@
-// App.js
-//
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Header } from './components/Header'
+import { Users } from './components/Users'
+import { DisplayBoard } from './components/DisplayBoard'
+import CreateUser from './components/CreateUser'
+import { getAllUsers, createUser } from './services/UserService'
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      flower: {},
-    };
+
+  state = {
+    user: {},
+    users: [],
+    numberOfUsers: 0
   }
 
-  componentDidMount() {
-    this.getFlower();
+  createUser = (e) => {
+      createUser(this.state.user)
+        .then(response => {
+          console.log(response);
+          this.setState({numberOfUsers: this.state.numberOfUsers + 1})
+      });
   }
 
-  async getFlower() {
-    const response = await fetch("/flower");
-    const data = await response.json();
-    this.setState({ flower: data });
+  getAllUsers = () => {
+    getAllUsers()
+      .then(users => {
+        console.log(users)
+        this.setState({users: users, numberOfUsers: users.length})
+      });
   }
+
+  onChangeForm = (e) => {
+      let user = this.state.user
+      if (e.target.name === 'firstname') {
+          user.firstName = e.target.value;
+      } else if (e.target.name === 'lastname') {
+          user.lastName = e.target.value;
+      } else if (e.target.name === 'email') {
+          user.email = e.target.value;
+      }
+      this.setState({user})
+  }
+
   render() {
+    
     return (
       <div className="App">
-        <h1>{this.state.flower.name}</h1>
-        <p>{this.state.flower.colour}</p>
+        <Header></Header>
+        <div className="container mrgnbtm">
+          <div className="row">
+            <div className="col-md-8">
+                <CreateUser 
+                  user={this.state.user}
+                  onChangeForm={this.onChangeForm}
+                  createUser={this.createUser}
+                  >
+                </CreateUser>
+            </div>
+            <div className="col-md-4">
+                <DisplayBoard
+                  numberOfUsers={this.state.numberOfUsers}
+                  getAllUsers={this.getAllUsers}
+                >
+                </DisplayBoard>
+            </div>
+          </div>
+        </div>
+        <div className="row mrgnbtm">
+          <Users users={this.state.users}></Users>
+        </div>
       </div>
     );
   }
 }
+
 export default App;
--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 import path from 'path';
 import express from "express";
+import bodyParser from 'body-parser';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 //Serve static files from client
 app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(bodyParser.json());
 
 //API
-app.get("/", (req, res) => {
-  res.send("just gonna send it");
+app.get('/api/users', (req, res) => {
+  console.log('api/users called!')
+  res.json(users);
 });
-app.get("/flower", (req, res) => {
-  res.json({
-    name: "Dandelion",
-    colour: "Blue-ish",
-  });
+
+app.post('/api/user', (req, res) => {
+  const user = req.body.user;
+  console.log('Adding user:::::', user);
+  users.push(user);
+  res.json("user addedd");
 });
+
+// app.get('/', (req,res) => {
+//   res.sendFile(path.join(__dirname, 'my-app', 'build', 'index.html'));
+//   // E:/Google Drive/Prosjekter/Software Development/ReactNode/react-nodejs-example/
+//   console.log("Root dirname: ", ___dirname);
+//   // res.sendFile('E:/Google Drive/Prosjekter/Software Development/ReactNode/react-nodejs-example/my-app/build/index.html'); // Denne måten å gjøre det på fungerer fint lokalt
+// });
 
 //SERVER
 app.listen(PORT, () => {

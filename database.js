1ec3c5fa9f5a0dbafc1175c6ca558b59ff6f11ec
arne90 <arneose@gmail.com>
2021-02-17 10:37:57 +0100

    la til comment

--- database.js
+++ database.js
@@ -3,6 +3,8 @@ const AWS = require("aws-sdk");
 AWS.config.update({ region: "eu-central-1" });
 const ddbDocumentClient = new AWS.DynamoDB.DocumentClient();
 
+console.log("Kommentar for commit?");
+
 const TABLE_NAME = "Salgssystem";
 
 export const getAllUsers = async (_) => {

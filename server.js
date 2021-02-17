57eb233af293e692f85043e075c2e4d3937f31a8
Martin Theodor Vasbotten <mtv@itverket.no>
2021-02-17 16:22:34 +0100

    hele requestbody er case som skal legges til

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.get("/api/case", async (_, res) => {
 
 app.post("/api/case", async (req, res) => {
   // console.debug("POST api/case called!");
-  const case_object = req.body.case_object;
-  await DataBase.saveCase(case_object);
+  await DataBase.saveCase(req.body);
   res.json("case added");
 });
 

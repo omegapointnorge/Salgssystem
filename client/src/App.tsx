7852900e62884db078ea40fba59d405287ae4606
Martin Theodor Vasbotten <mtv@itverket.no>
2021-02-26 14:10:05 +0100

    lagt til kort i app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Users } from "./components/Users";
 import DndColumns from "./components/dnd/DndColumns";
+import CardList from "./components/CardList/CardList";
 
 // interface user {
 //   firstName: string,
@@ -17,55 +18,16 @@ class App extends Component {
     numberOfUsers: 0,
   };
 
-  // componentDidMount() {
-  //   this.getAllUsers();
-  // }
-
-  // createUser = (e) => {
-  //   UserService.createUser(this.state.user).then((response) => {
-  //     this.setState({ numberOfUsers: this.state.numberOfUsers + 1 });
-  //   });
-  // };
-
-  // getAllUsers = () => {
-  //   UserService.getAllUsers().then((users) => {
-  //     this.setState({ users: users, numberOfUsers: users.length });
-  //   });
-  // };
-
-  // onChangeForm = (e) => {
-  //   let user = this.state.user<user>; // Hvordan få dette til å funke???
-  //   if (e.target.name === "firstname") {
-  //     user.firstName = e.target.value;
-  //   } else if (e.target.name === "lastname") {
-  //     user.lastName = e.target.value;
-  //   } else if (e.target.name === "email") {
-  //     user.email = e.target.value;
-  //   }
-  //   this.setState({ user });
-  // };
-
   render() {
     return (
       <div className="App">
         <Header></Header>
+        <CardList />
         <div className="container mrgnbtm">
           <div className="row">
             <div className="col-md-8">
-              {/* <CreateUser
-                user={this.state.user}
-                onChangeForm={this.onChangeForm}
-                createUser={this.createUser}
-              ></CreateUser>
-              <DragAndDropIndex/> */}
               <DndColumns />
             </div>
-            {/* <div className="col-md-4">
-              <DisplayBoard
-                numberOfUsers={this.state.numberOfUsers}
-                getAllUsers={this.getAllUsers}
-              ></DisplayBoard>
-            </div> */}
           </div>
         </div>
         <div className="row mrgnbtm">

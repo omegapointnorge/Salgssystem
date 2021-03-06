9b32b48758055984009ab5fcdf6085d908f35827
arne90 <arneose@gmail.com>
2021-03-06 12:08:41 +0100

    lagt til svg grafikk for itverket logo, endret css i header til å ikke bruke bootstrap + skalerer etter vw

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 import { Users } from "./components/Users";
 import DndColumns from "./components/dnd/DndColumns/DndColumns";
 
